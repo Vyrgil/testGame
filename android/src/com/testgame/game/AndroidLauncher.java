97f8bbb183ef091697a32e6b4af13c342db6a635
Salvatore Delpiano <delpiano.salvatore96@gmail.com>
2017-08-20 14:52:04 +0200

    First commit, default generated project

---
+++ android/src/com/testgame/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.testgame.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.testgame.game.testGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new testGame(), config);
+	}
+}
---
+++ html/src/com/testgame/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.testgame.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.testgame.game.testGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new testGame();
+        }
+}
